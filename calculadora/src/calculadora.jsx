e735530b9b44f09685ae580a11b1a51636a91d30
redondo08o <jesusre11d@gmail.com>
2023-02-02 09:02:30 -0500

    act_j

--- calculadora/src/calculadora.jsx
+++ calculadora/src/calculadora.jsx
@@ -3,110 +3,68 @@ import { useState } from "react"
 const Calculadora = ()=> {
 
     const [valor, setvalor] = useState(0)
-    
-    let n1; 
-    let op;
-    let n2 ;
-    let resul ;
+    const [valor1, setvalor1] = useState(0)
+    const [operador, setoperador] = useState(0)
 
-    const numero1 = () => {
-        if(valor == 0 ){
-        setvalor(valor + 1)
+    const numero = (e) => {
+        if(valor === 0 ){
+        setvalor(e.target.value)
         }
         else{
-            setvalor(valor + "1")
-        }
-    }
-    const numero2 = () => {
-        if(valor == 0 ){
-        setvalor(valor + 2)
-        }
-        else{
-            setvalor(valor + "2")
-        }
-    }
-    const numero3 = () => {
-        if(valor == 0 ){
-        setvalor(valor + 3)
-        }
-        else{
-            setvalor(valor + "3")
-        }
-    }
-    const numero4 = () => {
-        if(valor == 0 ){
-        setvalor(valor + 4)
-        }
-        else{
-            setvalor(valor + "4")
-        }
-    }
-    const numero5 = () => {
-        if(valor == 0 ){
-        setvalor(valor + 5)
-        }
-        else{
-            setvalor(valor + "5")
-        }
-    }
-    const numero6 = () => {
-        if(valor == 0 ){
-        setvalor(valor + 6)
-        }
-        else{
-            setvalor(valor + "6")
-        }
-    }
-    const numero7 = () => {
-        if(valor == 0 ){
-        setvalor(valor + 7)
-        }
-        else{
-            setvalor(valor + "7")
-        }
-    }
-    const numero8 = () => {
-        if(valor == 0 ){
-        setvalor(valor + 8)
-        }
-        else{
-            setvalor(valor + "8")
-        }
-    }
-    const numero9 = () => {
-        if(valor == 0 ){
-        setvalor(valor + 9)
-        }
-        else{
-            setvalor(valor + "9")
-        }
-    }
-    const numero0 = () => {
-        if(valor == 0 ){
-        setvalor(valor + 0)
-        }
-        else{
-            setvalor(valor + "0")
+            setvalor(valor + e.target.value)
         }
     }
 
-    const suma = ()=> {
-        n1 = parseInt(valor)
-        op = "+"
-        valor = 0
+    const opera = (e)=> {
+        if (operador == 0) {
+            setvalor1(parseFloat(valor))
+            setoperador(e.target.value)
+            setvalor(0) 
+        }
+        else{
+            setoperador(e.target.value)
+        }
+       
+    }
+
+    const borrar= ()=>{
+        setvalor(0)
+        setoperador(0)
+    }
+
+    const punto = ()=>{
+        
+            
+                setvalor(valor + '.')
+            
     }
 
     const res = () => {
-        n2 = parseInt(valor) 
-        if(op == "+"){
-            
-            resul = n1 + n2
+
+        if(operador == "+"){
+            setvalor(parseFloat(valor) + valor1)
+            setoperador(0)
         }
+        if(operador == "-"){
+            setvalor(valor1 - parseFloat(valor))
+            setoperador(0)
+        }
+        if(operador == "/"){
+            setvalor(valor1 / parseFloat(valor) )
+            setoperador(0)
+        }
+        if(operador == "*"){
+            setvalor(valor1 * parseFloat(valor))
+            setoperador(0)
+        }
+        
     }
+    
 
 
     return (
         <>
+        
         <div class="calculator">
     
             <div class="console">
@@ -117,24 +75,24 @@ const Calculadora = ()=> {
                 <div class="currentNumber"></div>
             </div>
             <div class="keys">
-                <button class="clear">C</button>
+                <button class="clear" onClick={borrar}>C</button>
                 <button class="operator">x^</button>
-                <button class="operator">/</button>
-                <button class="operator">*</button>
-                <button class="number"onClick={numero7}>7</button>
-                <button class="number"onClick={numero8}>8</button>
-                <button class="number"onClick={numero9}>9</button>
-                <button class="operator">-</button>
-                <button class="number"onClick={numero4}>4</button>
-                <button class="number"onClick={numero5}>5</button>
-                <button class="number"onClick={numero6}>6</button>
-                <button class="operator" onClick={suma}>+</button>
-                <button class="number" onClick={numero1} >1</button>
-                <button class="number" onClick={numero2}>2</button>
-                <button class="number" onClick={numero3}>3</button>
-                <button class="equals">=</button>
-                <button class="number"onClick={numero0} id="zero">0</button>
-                <button class="number">.</button>
+                <button class="operator" onClick={opera} value={"/"}>/</button>
+                <button class="operator" onClick={opera} value={"*"}>*</button>
+                <button class="number"onClick={numero}value={7}>7</button>
+                <button class="number"onClick={numero} value={8}>8</button>
+                <button class="number"onClick={numero}value={9}>9</button>
+                <button class="operator" onClick={opera} value={"-"}>-</button>
+                <button class="number"onClick={numero}value={4}>4</button>
+                <button class="number"onClick={numero}value={5}>5</button>
+                <button class="number"onClick={numero} value={6}>6</button>
+                <button class="operator" onClick={opera} value={"+"}>+</button>
+                <button class="number" onClick={numero} value={1} >1</button>
+                <button class="number" onClick={numero} value={2}>2</button>
+                <button class="number" onClick={numero} value={3}>3</button>
+                <button class="equals" onClick={res}>=</button>
+                <button class="number"onClick={numero} value={0} id="zero">0</button>
+                <button class="number" onClick={punto} >.</button>
             </div>
         </div>
         </>
